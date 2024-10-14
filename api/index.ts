3e524f4173a0bd8556e68687a3b7bb42841532fd
alex.kopachov <alex.kopachov@gmail.com>
2024-10-14 22:50:37 +0200

    Vercel edge runtime

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import { handle } from 'hono/vercel';
 import app from '../src/app.js';
 
-export const runtime = 'edge';
+export const config = {
+  runtime: 'edge',
+};
 
 export const GET = handle(app);
