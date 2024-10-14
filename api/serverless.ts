cdad3f0c3f9e79dcb91b4f0647003972afc1d36e
alex.kopachov <alex.kopachov@gmail.com>
2024-10-14 16:21:07 +0200

    Again

--- api/serverless.ts
+++ api/serverless.ts
@@ -4,7 +4,9 @@ const app = Fastify({
   logger: true,
 });
 
-app.register(import('../src/app'));
+app.register(import('../app.js'), {
+  prefix: '/',
+});
 
 export default async (req: FastifyRequest, res: FastifyReply) => {
   await app.ready();
