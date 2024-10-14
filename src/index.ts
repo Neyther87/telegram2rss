31048d83cd21b2ead428bef3bbb1a452f753a068
alex.kopachov <alex.kopachov@gmail.com>
2024-10-14 15:42:36 +0200

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,46 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyInstance } from 'fastify';
 import { getChannelInfoWithPosts } from './telegram-parser';
 import { Readable } from 'stream';
 import { buildFeed } from './telegram-to-feed';
 
-const app = Fastify({
-  logger: true,
-});
-
-app.get(
-  '/rss/:channel',
-  {
-    schema: {
-      params: {
-        type: 'object',
-        properties: {
-          channel: { type: 'string' },
+export default function registerRoutes(app: FastifyInstance) {
+  app.get(
+    '/rss/:channel',
+    {
+      schema: {
+        params: {
+          type: 'object',
+          properties: {
+            channel: { type: 'string' },
+          },
+          required: ['channel'],
         },
-        required: ['channel'],
-      },
-      querystring: {
-        type: 'object',
-        properties: {
-          count: { type: 'number' },
+        querystring: {
+          type: 'object',
+          properties: {
+            count: { type: 'number' },
+          },
         },
       },
     },
-  },
-  async (request, response) => {
-    const { channel } = request.params as { channel: string };
-    const { count: postsCountRaw } = request.query as { count?: number | string };
-    const postsCount = postsCountRaw ? Math.min(Number(postsCountRaw), 50) : undefined;
-    const channelInfo = await getChannelInfoWithPosts(channel, { count: postsCount });
-    const stream = new Readable();
-    stream._read = () => {};
-    await buildFeed(channelInfo, stream);
-    return response.status(200).type('application/rss+xml').send(stream);
-  },
-);
-
-export default async function handler(req: any, res: any) {
-  await app.ready();
-  app.server.emit('request', req, res);
+    async (request, response) => {
+      const { channel } = request.params as { channel: string };
+      const { count: postsCountRaw } = request.query as { count?: number | string };
+      const postsCount = postsCountRaw ? Math.min(Number(postsCountRaw), 50) : undefined;
+      const channelInfo = await getChannelInfoWithPosts(channel, { count: postsCount });
+      const stream = new Readable();
+      stream._read = () => {};
+      await buildFeed(channelInfo, stream);
+      return response.status(200).type('application/rss+xml').send(stream);
+    },
+  );
 }
 
 if (process.env.NODE_ENV === 'development') {
+  const app = Fastify({
+    logger: true,
+  });
+  registerRoutes(app);
   app.listen({ port: 8080 }, (err, address) => {
     if (err) {
       console.error(err);
